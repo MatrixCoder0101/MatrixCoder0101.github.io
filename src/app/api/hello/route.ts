4c39c5f4c3f46c550815757bc4de804e880b9813
Goutam Kumar <tkgoutamm625@gmail.com>
2023-11-15 08:18:10 +0530

    update src/app/api/hello/route.ts

--- src/app/api/hello/route.ts
+++ src/app/api/hello/route.ts
@@ -1,6 +1,12 @@
 import { NextRequest, NextResponse } from "next/server";
 
 export function GET(request: NextRequest) {
- //const url = request.nextUrl;
- return NextResponse.json({ message: "Hello, World!" });
+  const jsonResponse = { message: "Hello, World!" };
+
+  // Set Content-Disposition header to inline for displaying response in the browser
+  return NextResponse.json(jsonResponse, {
+    headers: {
+      "Content-Disposition": 'inline; filename="response.json"',
+    },
+  });
 }

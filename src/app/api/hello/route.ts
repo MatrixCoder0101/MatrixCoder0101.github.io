6c52701bdc1340ea1cee014a3f7531a52ccdcd2b
Goutam Kumar <tkgoutamm625@gmail.com>
2023-11-15 08:09:55 +0530

    update src/app/api/hello/route.ts

--- src/app/api/hello/route.ts
+++ src/app/api/hello/route.ts
@@ -1,6 +1,6 @@
 import { NextRequest, NextResponse } from "next/server";
 
 export function GET(request: NextRequest) {
- const url = request.nextUrl;
- return NextResponse.json({ HelloWorld: url });
+ //const url = request.nextUrl;
+ return NextResponse.json({ message: "Hello, World!" });
 }

de1f455c16468322cb37fb5dee339a65b6838672
Goutam Kumar <tkgoutamm625@gmail.com>
2023-11-15 07:41:32 +0530

    update src/app/api/hello/route.ts

--- src/app/api/hello/route.ts
+++ src/app/api/hello/route.ts
@@ -1,3 +1,15 @@
-export default function handler(req, res) {
-  res.status(200).json({ text: 'Hello' });
+// app/api/sendJson.ts
+import { Request, Response } from 'next';
+
+export async function GET(request: Request) {
+  const jsonData = {
+    message: "Hello, this is a JSON response!",
+    timestamp: new Date().toISOString(),
+  };
+
+  return new Response(JSON.stringify(jsonData), {
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
 }

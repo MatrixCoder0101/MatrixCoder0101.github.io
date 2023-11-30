3a4d8a4b3d058de5b677728622f1a2f7ea3b3158
Goutam Kumar <tkgoutamm625@gmail.com>
2023-11-30 09:54:48 +0530

    update src/app/api/hello/route.ts

--- src/app/api/hello/route.ts
+++ src/app/api/hello/route.ts
@@ -1,6 +1,6 @@
 import { NextRequest, NextResponse } from "next/server";
 
-export function middleware(request) {
+export function hello(request) {
   const jsonResponse = { message: "Hello, World!" };
 
   return NextResponse.json({ message: "Hello, World!" });

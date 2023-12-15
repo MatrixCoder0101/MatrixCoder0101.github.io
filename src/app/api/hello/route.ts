92ea79acba0fd83b941683dc8a1511985839d43c
Goutam Kumar <tkgoutamm625@gmail.com>
2023-12-15 15:15:16 +0530

    update src/app/api/hello/route.ts

--- src/app/api/hello/route.ts
+++ src/app/api/hello/route.ts
@@ -1,9 +1,7 @@
 import { NextResponse } from 'next/server'
  
 export async function GET(request: Request) {
-  const { searchParams } = new URL(request.url)
-  const name = searchParams.get('name') 
-  const id = searchParams.get('id')
-  console.log(name, id)
-  return NextResponse.json({ name, id }, { status: 500 })
+  const { searchParams } = new URL(request.url);
+  const name = searchParams.get('name');
+  return NextResponse.json({ name }, { status: 500 })
 }

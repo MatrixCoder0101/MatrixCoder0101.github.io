92c5540e167851e1056c544d72a6802febef1bea
Goutam Kumar <tkgoutamm625@gmail.com>
2023-12-16 10:44:07 +0530

    update src/app/chatgpt/page.tsx

--- src/app/chatgpt/page.tsx
+++ src/app/chatgpt/page.tsx
@@ -1,60 +1,68 @@
 //ChatBot
 "use client"
 
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import axios from 'axios';
 
 const TypewriterEffect = ({ text }: { text: string }) => {
   const [displayText, setDisplayText] = useState('');
 
-  const typeWriter = async () => {
-    for (let i = 0; i < text.length; i++) {
-      setDisplayText((prevText) => prevText + text.charAt(i));
-      await new Promise((resolve) => setTimeout(resolve, 50)); // Adjust the delay as needed
-    }
-  };
+  useEffect(() => {
+    const typeWriter = async () => {
+      for (let i = 0; i < text.length; i++) {
+        setDisplayText((prevText) => prevText + text.charAt(i));
+        await new Promise((resolve) => setTimeout(resolve, 50)); // Adjust the delay as needed
+      }
+    };
 
-  return (
-    <div>
-      <p>{displayText}</p>
-      {displayText.length === text.length || (
-        <button onClick={typeWriter}>Show Answer</button>
-      )}
-    </div>
-  );
+    typeWriter();
+  }, [text]);
+
+  return <p>{displayText}</p>;
 };
 
 const Chatbot = () => {
   const [userQuery, setUserQuery] = useState('');
+  const [chatHistory, setChatHistory] = useState<string[]>([]);
   const [answer, setAnswer] = useState('');
 
   const handleAskQuestion = async () => {
+    if (userQuery.trim() === '') return;
+
     try {
-      const response = await axios.get(`/api/bard?query=${userQuery}`);
+      setChatHistory((prevHistory) => [...prevHistory, `You: ${userQuery}`]);
+      const response = await axios.get(`../api/bard?query=${userQuery}`);
       const ans = response.data.ans || 'No answer found';
       setAnswer(ans);
+      setChatHistory((prevHistory) => [...prevHistory, `ChatBot: ${ans}`]);
     } catch (error) {
       console.error(error);
       setAnswer('Internal Server Error');
+      setChatHistory((prevHistory) => [...prevHistory, 'ChatBot: Internal Server Error']);
     }
+    setUserQuery('');
   };
 
   return (
     <div className="container mx-auto p-4">
       <h1 className="text-4xl font-bold mb-4">Chatbot UI</h1>
       <div className="mb-4">
+        {chatHistory.map((message, index) => (
+          <div key={index} className={index % 2 === 0 ? 'text-left' : 'text-right'}>
+            {message}
+          </div>
+        ))}
         <TypewriterEffect text={answer || 'Ask me a question!'} />
       </div>
       <div className="flex">
-        <input
-          type="text"
+        <textarea
           value={userQuery}
           onChange={(e) => setUserQuery(e.target.value)}
           className="border p-2 mr-2"
-          placeholder="Ask a question"
+          placeholder="Type your question"
         />
         <button onClick={handleAskQuestion} className="bg-blue-500 text-white p-2">
-          Ask Question
+          Send
         </button>
       </div>
     </div>
@@ -62,3 +70,4 @@ const Chatbot = () => {
 };
 
 export default Chatbot;
+

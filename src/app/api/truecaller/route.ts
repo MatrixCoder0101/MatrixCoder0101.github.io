b5f16507cba187871c05207672242a07a532dab3
Goutam Kumar <tkgoutamm625@gmail.com>
2023-12-16 20:43:43 +0530

    update src/app/api/truecaller/route.ts

--- src/app/api/truecaller/route.ts
+++ src/app/api/truecaller/route.ts
@@ -1,17 +1,17 @@
 import { NextResponse } from "next/server";
-import axios from 'axios';
+import axios from "axios";
 
 export async function GET(request: Request) {
-    const { searchParams } = new URL(request.url)
-    const phone = searchParams.get("phone")
-    const id = searchParams.get("id")
+    const { searchParams } = new URL(request.url);
+    const phone = searchParams.get("phone");
+    const id = searchParams.get("id");
 
-   const apiUrl = `https://supreme-catfish-goutammallick516.koyeb.app/truecaller?phone=${phone}&id=${id}`;
-   let response = await axios.get(apiUrl);
-   let json = response.data;
-   if (json) {
-    return NextResponse.json(json, { status: 500 });
-  } else {
-    return NextResponse.json({ error: 'Kuch Gadbad he'}, { status: 500 });
-  }
+    const apiUrl = `https://supreme-catfish-goutammallick516.koyeb.app/truecaller?phone=${phone}&id=${id}`;
+    let response = await axios.get(apiUrl);
+    let json = response.data;
+    if (json) {
+        return NextResponse.json(json, { status: 500 });
+    } else {
+        return NextResponse.json({ error: "Kuch Gadbad he" }, { status: 500 });
+    }
 }

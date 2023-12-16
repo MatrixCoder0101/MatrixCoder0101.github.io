354e1888d85ee162b39e5d7a829ec01907fd71e8
Goutam Kumar <tkgoutamm625@gmail.com>
2023-12-16 10:58:23 +0530

    update src/app/chatgpt/page.tsx

--- src/app/chatgpt/page.tsx
+++ src/app/chatgpt/page.tsx
@@ -1,24 +1,27 @@
 //ChatBot
 "use client"
 
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import axios from 'axios';
 
 const TypewriterEffect = ({ text }: { text: string }) => {
   const [displayText, setDisplayText] = useState('');
 
-  useEffect(() => {
-    const typeWriter = async () => {
-      for (let i = 0; i < text.length; i++) {
-        setDisplayText((prevText) => prevText + text.charAt(i));
-        await new Promise((resolve) => setTimeout(resolve, 50)); // Adjust the delay as needed
-      }
-    };
+  const typeWriter = async () => {
+    for (let i = 0; i < text.length; i++) {
+      setDisplayText((prevText) => prevText + text.charAt(i));
+      await new Promise((resolve) => setTimeout(resolve, 50)); // Adjust the delay as needed
+    }
+  };
 
-    typeWriter();
-  }, [text]);
-
-  return <p>{displayText}</p>;
+  return (
+    <div>
+      <p>{displayText}</p>
+      {displayText.length === text.length || (
+        <button onClick={typeWriter}>Show Answer</button>
+      )}
+    </div>
+  );
 };
 
 const Chatbot = () => {
@@ -27,20 +30,36 @@ const Chatbot = () => {
   const [answer, setAnswer] = useState('');
 
   const handleAskQuestion = async () => {
-    if (userQuery.trim() === '') return;
-
     try {
+      if (userQuery.trim() === '') return;
+
       setChatHistory((prevHistory) => [...prevHistory, `You: ${userQuery}`]);
-      const response = await
-      axios.get(`https://matrix-coder.vercel.app/api/bard?query=${userQuery}`);
+
+      const response = await axios.get(`/api/bard?query=${encodeURIComponent(userQuery)}`);
       const ans = response.data.ans || 'No answer found';
+
       setAnswer(ans);
       setChatHistory((prevHistory) => [...prevHistory, `ChatBot: ${ans}`]);
     } catch (error) {
-      console.error(error);
+      console.error('Axios Error:', error);
+
+      if (error.response) {
+        // The request was made and the server responded with a status code
+        console.error('Response Data:', error.response.data);
+        console.error('Response Status:', error.response.status);
+        console.error('Response Headers:', error.response.headers);
+      } else if (error.request) {
+        // The request was made but no response was received
+        console.error('Request Data:', error.request);
+      } else {
+        // Something happened in setting up the request that triggered an Error
+        console.error('Error Message:', error.message);
+      }
+
       setAnswer('Internal Server Error');
       setChatHistory((prevHistory) => [...prevHistory, 'ChatBot: Internal Server Error']);
     }
+
     setUserQuery('');
   };
 
@@ -71,4 +90,3 @@ const Chatbot = () => {
 };
 
 export default Chatbot;
-

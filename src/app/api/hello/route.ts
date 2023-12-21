a5e629464a36fa492dce34c5f2275dae0848e726
Goutam Kumar <tkgoutamm625@gmail.com>
2023-12-21 10:21:26 +0530

    Delete src/app/api directory

--- src/app/api/hello/route.ts
+++
@@ -1,8 +0,0 @@
-import { NextResponse } from 'next/server'
- 
-export async function GET(request: Request) {
-  const { searchParams } = new URL(request.url)
-  const name = searchParams.get('name') 
-  console.log(name)
-  return NextResponse.json({ name }, { status: 500 })
-}

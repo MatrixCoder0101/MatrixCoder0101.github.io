9a225fea92fb9ec97a83b18f0ab7faff4338abc4
Goutam Kumar <tkgoutamm625@gmail.com>
2023-12-21 15:39:53 +0530

    update src/app/page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@
 import Image from "next/image";
 import { Inter } from 'next/font/google'
 
-import { Hero, Navbar, Social, Footer, Globe, Projects, Tawk } from "../components"
+import { Hero, Navbar, Social, Footer, Globe, Projects, Tawk, MatrixRainingCode } from "../components"
 
 export default function Home() {
   return (

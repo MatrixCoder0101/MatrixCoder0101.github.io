159d68ac05c300c13319deddf22eef2af31640b4
Goutam Kumar <tkgoutamm625@gmail.com>
2023-12-27 22:11:32 +0530

    update src/app/api/bio/route.ts

--- src/app/api/bio/route.ts
+++ src/app/api/bio/route.ts
@@ -1,8 +1,5 @@
 import { NextResponse } from 'next/server'
  
 export async function GET(request: Request) {
-  //const { searchParams } = new URL(request.url)
-  //const name = searchParams.get('name') 
-  //console.log(name)
-  return NextResponse.json({ name: 'Goutam' }, { status: 500 })
+  return NextResponse.json({ message: 'Hello from Goutam' }, { status: 500 })
 }

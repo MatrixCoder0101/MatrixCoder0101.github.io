51cc7fe7c849fcae30221663f77aa24a9462b849
Goutam Kumar <tkgoutamm625@gmail.com>
2024-01-02 17:18:33 +0530

    update src/components/Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,9 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 
 export default function Navbar() {
   return (
-      <header className="glassmorphism-header bg-opacity-70 backdrop-blur-lg rounded-lg p-2 text-white fixed w-full top-0 z-60 flex flex-col items-center justify-center">
-        <b className="text-2xl font-bold">Matrix Coder</b>{" "}
-      </header>
+    <header className="glassmorphism-header backdrop-filter backdrop-blur-lg bg-white bg-opacity-10 rounded-lg p-2 text-white fixed w-full top-0 z-60 flex flex-col items-center justify-center">
+      <b className="text-2xl font-bold">Matrix Coder</b>{" "}
+    </header>
   );
 };

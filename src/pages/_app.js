d5d4cb1b3df69c6db9c9c5cce77bac2b9fb4e0e0
Goutam Kumar <tkgoutamm625@gmail.com>
2024-01-06 18:08:56 +0530

    update src/pages/_app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -5,6 +5,7 @@ import NextNProgress from 'nextjs-progressbar';
 export default function App({ Component, pageProps }) {
   return (
     <>
+      <LoadingScreen />
       <NextNProgress color="#15ff8eda" startPosition={0.3} stopDelayMs={200} height={3} showOnShallow={true} />
       <Navbar />
       <Component {...pageProps} />
